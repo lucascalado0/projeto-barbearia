8359a830040aadda300934946ebcc5928f5174bb
devlucascalado <devlucascalado@gmail.com>
2025-03-08 23:41:26 -0300

    commit inicial

---
+++ src/app/clients/edit-clients/edit-clients.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-clients',
+  imports: [],
+  templateUrl: './edit-clients.component.html',
+  styleUrl: './edit-clients.component.scss'
+})
+export class EditClientsComponent {
+
+}
